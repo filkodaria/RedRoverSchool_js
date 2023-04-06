e4e2d76a14b7692b698cf36131e91c85133e2bf9
filkodaria <filko_d@ukr.net>
2023-04-06 08:46:15 +0300

    HW_4 edited

--- HW_4.js
+++ HW_4.js
@@ -1,8 +1,8 @@
-//BLOCK_1
+// BLOCK_1
 
-// TASK_1 - Rhombus_numeric (<=9 rows)
+// TASK_1 - Rhombus_numeric
 
-let heightRhomb = 9;
+let heightRhomb = 11;
 
 for (let i = 1; i <= heightRhomb; i++) {
   let lineRhomb = "";
@@ -10,10 +10,15 @@ for (let i = 1; i <= heightRhomb; i++) {
     lineRhomb += " ";
   }
   for (let k = 1; k <= i; k++) {
+    if (k >= 10) {
+      lineRhomb += k % 10;
+    } else
     lineRhomb += k;
 	}
-	for (let l = i - 1; l > 0; l--) {
-    lineRhomb += l;
+  for (let l = i - 1; l >= 1; l--) {
+    if (l >= 10) {
+      lineRhomb += l % 10;
+    } else lineRhomb += l;
   }
   console.log(lineRhomb);
 }
@@ -23,10 +28,14 @@ for (let i = heightRhomb - 1; i >= 1; i--) {
     lineRhomb += " ";
   }
   for (let k = 1; k <= i; k++) {
-    lineRhomb += k;
+    if (k >= 10) {
+      lineRhomb += k % 10;
+    } else lineRhomb += k;
 	}
-	for (let l = i - 1; l > 0; l--) {
-    lineRhomb += l;
+  for (let l = i - 1; l >= 1; l--) {
+    if (l >= 10) {
+      lineRhomb += l % 10;
+    } else lineRhomb += l;
   }
   console.log(lineRhomb);
 }
@@ -34,6 +43,7 @@ for (let i = heightRhomb - 1; i >= 1; i--) {
 
 // TASK_2 - Tree
 
+// variant_1
 let heightTree = 7;
 
 for (let i = 1; i <= heightTree; i++) {
@@ -47,3 +57,39 @@ for (let i = 1; i <= heightTree; i++) {
   console.log(lineTree);
 }
 
+// variant_2
+let hTree = 5;
+let space = ' ';
+let star = '*';
+let sum = 1;
+
+for (let i = 1; i <= hTree; i++) {
+  let lTree = '';
+  lTree += space.repeat(hTree - i) + star.repeat(sum);
+  sum += 2;
+  console.log(lTree);
+}
+
+
+// TASK_3 - Triangle_numeric
+
+let heightTrngl = 5;
+
+for (let i = 1; i <= heightTrngl; i++) {
+  let num = i;
+  let line = '';
+  let space = ' ';
+  
+  for (let j = 1; j <= i; j++) {
+    line += num + space;
+    num = num + (heightTrngl - j);
+	}
+  console.log(line);
+}
+
+
+// TASK_4 - Sandwich_bar
+
+
+
+// BLOCK_2
